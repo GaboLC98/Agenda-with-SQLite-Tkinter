e1cb599f3d87042b915bcd3b83aef8336c9928b5
Gabriel Lopez Calderon <69644400+GaboLC98@users.noreply.github.com>
2021-08-20 15:55:24 -0300

    Update agenda.py
    Update to work with data base / Actualizado para trabajar con base de datos

--- agenda.py
+++ agenda.py
@@ -1,91 +1,59 @@
-from io import open
+import sqlite3
 print(""" █████╗  ██████╗ ███████╗███╗   ██╗██████╗  █████╗ 
 ██╔══██╗██╔════╝ ██╔════╝████╗  ██║██╔══██╗██╔══██╗
 ███████║██║  ███╗█████╗  ██╔██╗ ██║██║  ██║███████║
 ██╔══██║██║   ██║██╔══╝  ██║╚██╗██║██║  ██║██╔══██║
 ██║  ██║╚██████╔╝███████╗██║ ╚████║██████╔╝██║  ██║
 ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝""")
+conexion=sqlite3.connect("agenda.db")
+cursor=conexion.cursor()
 def start():
-    ficherol=open("Lunes.txt","a")
-    ficherom=open("Martes.txt","a")
-    ficheromi=open("Miercoles.txt","a")
-    ficheroj=open("Jueves.txt","a")
-    ficherov=open("Viernes.txt","a")
-    ficherol1=open("Lunes.txt","r")
-    leer_lunes=ficherol1.readlines()
-    ficherol1.close()
-    ficherom1=open("Martes.txt","r")
-    leer_martes=ficherom1.readlines()
-    ficherom1.close()
-    ficheromi1=open("Miercoles.txt","r")
-    leer_miercoles=ficheromi1.readlines()
-    ficheromi1.close()
-    ficheroj1=open("Jueves.txt","r")
-    leer_jueves=ficheroj1.readlines()
-    ficheroj1.close()
-    ficherov1=open("Viernes.txt","r")
-    leer_viernes=ficherov1.readlines()
-    ficherov1.close()
     opcion=int(input("\nSeleccione el nro de accion a realizar:\n1-Agregar a la agenda\n2-Eliminar de la agenda\n3-Ver agenda\n-----> "))
     while opcion:
         if opcion==1:
             dia_op1=int(input("¿A que dia desea agregar una tarea?\n1-Lunes\n2-Martes\n3-Miercoles\n4-Jueves\n5-Viernes\n"))
-            tarea=input("Ingrese la tarea con la fecha de entrega: ")
+            tarea=input("Ingrese la tarea: ")
+            entrega=input("Ingrese la fecha de entrega: ")
             if dia_op1==1:
-                ficherol.write(tarea+"\n")
-                ficherol.close()
+                cursor.execute(f'INSERT INTO TAREAS VALUES(NULL,"Lunes","{tarea}","{entrega}")')
             elif dia_op1==2:
-                ficherom.write(tarea+"\n")
-                ficherom.close()
+                cursor.execute(f'INSERT INTO TAREAS VALUES(NULL,"Martes","{tarea}","{entrega}")')
             elif dia_op1==3:
-                ficheromi.write(tarea+"\n")
-                ficheromi.close()
+                cursor.execute(f'INSERT INTO TAREAS VALUES(NULL,"Miercoles","{tarea}","{entrega}")')
             elif dia_op1==4:
-                ficheroj.write(tarea+"\n")
-                ficheroj.close()
+                cursor.execute(f'INSERT INTO TAREAS VALUES(NULL,"Jueves","{tarea}","{entrega}")')
             elif dia_op1==5:
-                ficherov.write(tarea+"\n")
-                ficherov.close()
+                cursor.execute(f'INSERT INTO TAREAS VALUES(NULL,"Viernes","{tarea}","{entrega}")')
             else:
-                print("Opcion incorrecta, vuelva a intentarlo")
-            start()
-        elif opcion==2:
-            def del_line():
-                for line in lineas:
-                    if line!=leer_lunes[eliminar]:
-                        fichero.write(line)
-                fichero.close()
+                print("Opcion incorrecta, vuelva a intentarlo\n")
                 start()
-            numero=int(input("En que dia desea eliminar:\n1-Lunes\n2-Martes\n3-Miercoles\n4-Jueves\n5-Viernes\n-----> "))
-            if numero==1:
-                print(f"TAREAS: {leer_lunes}")                
-                fichero=open("Lunes.txt","w")
-                lineas=leer_lunes
-            elif numero==2:
-                print(f"TAREAS: {leer_martes}")
-                fichero=open("Martes.txt","w")
-                lineas=leer_miercoles             
-            elif numero==3:
-                print(f"TAREAS: {leer_miercoles}")
-                fichero=open("Miercoles.txt","w")
-                lineas=leer_jueves               
-            elif numero==4:
-                print(f"TAREAS: {leer_jueves}")
-                fichero=open("Jueves.txt","w")
-                lineas=leer_viernes               
-            elif numero==5:
-                print(f"TAREAS: {leer_viernes}")
-                fichero=open("Viernes.txt","w")
-                lineas=leer_viernes
+            conexion.commit()
+            start()
+        if opcion==2:
+            dia_op1=int(input("En que dia desea eliminar:\n1-Lunes\n2-Martes\n3-Miercoles\n4-Jueves\n5-Viernes\n"))
+            if dia_op1==1:
+                cursor.execute('SELECT * FROM TAREAS WHERE DIA="Lunes"')
+            elif dia_op1==2:
+                cursor.execute('SELECT * FROM TAREAS WHERE DIA="Martes"')
+            elif dia_op1==3:
+                cursor.execute('SELECT * FROM TAREAS WHERE DIA="Miercoles"')
+            elif dia_op1==4:
+                cursor.execute('SELECT * FROM TAREAS WHERE DIA="Jueves"')    
+            elif dia_op1==5:
+                cursor.execute('SELECT * FROM TAREAS WHERE DIA="Viernes"')   
             else:
-                print("Opcion incorrecta, vuelva a intentarlo")
+                print("Opcion incorrecta, vuelva a intentarlo\n")
                 start()
-            eliminar=int(input("¿Que tarea desea eliminar? (nros del 0 a n) ------> "))
-            del_line()     
-        elif opcion==3:
-            print(f"LUNES: {leer_lunes}\nMARTES: {leer_martes}\nMIERCOLES: {leer_miercoles}\nJUEVES: {leer_jueves}\nVIERNES: {leer_viernes} ")
+            eliminar=cursor.fetchall()
+            print(eliminar)
+            id_eliminar=int(input('¿Que tarea desea eliminar? (seleccione la ID)'))
+            cursor.execute(f'DELETE FROM TAREAS WHERE ID={id_eliminar}')
+            conexion.commit()
             start()
-        else:
-            print("Opcion incorrecta, vuelva a intentarlo") 
+        if opcion==3:
+            cursor.execute('SELECT * FROM TAREAS')
+            ver=cursor.fetchall()
+            print(ver)
             start()
-start()
+start()  
+conexion.close()
